0b41e22373a4b6fbca22ce9aa15f98fc527dbd24
deepak20k5 <deepak.k@allyra.ai>
2026-09-23 03:04:50 +0530

    Deploy Phase 10 Dynamic Recalibration Engine & Missed-Dose Advisor to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "340764986"
+    serviceWorkerVersion: "4041413793"
   }
 });
